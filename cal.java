dc26af05726605ca9028dc186eee7362337fe82e
vinaygautam93 <vinaygautam935680@gmail.com>
2025-01-07 16:13:18 +0530

    addotion ho raha hai

--- cal.java
+++ cal.java
@@ -3,5 +3,18 @@ public class cal {
    public static void main( String [] args) {
             
        System.out.println("welcome");
+       Scanner sc = new Scanner;
+       int x = sc.nextInt();
+       int y = sc.nextInt();
+
+       int add = addition(x,y);
+
    }
+
+   public int addition(int x , int y) 
+   { 
+      return x+y;
+   }
+   
+   
 }
